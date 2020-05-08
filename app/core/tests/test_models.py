b5f84a41af879716b294366c499e68355aa2bb11
Ed Powderham <edpowderham@outlook.com>
2020-05-08 13:39:16 +0200

    fix: linting test_models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,8 @@ class ModelTests(TestCase):
         """Test creating new user with an email is successful"""
         email = "test@edpowderham.com"
         password = "Testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model() \
+            .objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
