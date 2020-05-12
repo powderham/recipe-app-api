6c9dbca27c5c1d69174f318c4fc06336596c34a8
Ed Powderham <ed.powderham@TK685.local>
2020-05-12 11:23:58 +0200

    feat: add create tag

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='ed@test.com', password="testpass"):
+    """ Create a sample user """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -30,3 +36,9 @@ class ModelTests(TestCase):
             'test@edpowderham.com', 'test123')
 
         self.assertTrue(user.is_superuser, user.is_staff)
+
+    def test_tag_str(self):
+        """ Test the tag string representation """
+        tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
+
+        self.assertEqual(str(tag), tag.name)
